c0937caf10b2f79e75407e3481126091b18ac00d
Osman0202 <Osmanpanpanza@yandex.ru>
2023-02-03 17:05:36 +0600

    'hw7'

--- internetshop/urls.py
+++ internetshop/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from products.views import main, products_view, product_detail_view, categories_view, create_product_view
+from products.views import main, products_view, product_detail_view, categories_view, create_product_view, main_view
 from django.conf.urls.static import static
 from internetshop.settings import MEDIA_URL, MEDIA_ROOT
 from users.views import login_view, logout_view, register_view
@@ -10,18 +10,16 @@ urlpatterns = [
     path('', main),
     path('admin/', admin.site.urls),
     path('products/', products_view),
-    # path('main_view/', main_view),
+    path('main_view/', main_view),
     path('products/<int:id>/', product_detail_view),
     path('categories/', categories_view),
     path('products/create/', create_product_view),
 
-    # users
+    #users
+
     path('users/login/', login_view),
     path('users/logout/', logout_view),
     path('users/register/', register_view)
-
-
-
 ]
 
 urlpatterns += static(MEDIA_URL, document_root= MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -4,26 +4,41 @@ from products.forms import ProductCreateForm, CommentCreateForm
 
 
 # Create your views here.
+PAGINATION_LIMIT = 3
 
 
 def main(request):
     if request.method == 'GET':
         return render(request, 'layosts/index.html')
 
-
 def products_view(request):
     if request.method == 'GET':
+        products = Product.objects.all()
         category_id = request.GET.get('category')
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
+        if search is not None:
+            products = Product.objects.filter(
+                title__icontains=search
+            )
+
+        max_page = products.__len__() / PAGINATION_LIMIT
+        if round(max_page) < max_page:
+            max_page = round(max_page) + 1
+        else:
+            max_page = round(max_page)
+
+        products = products[PAGINATION_LIMIT * (page - 1):PAGINATION_LIMIT * page]
+
         if category_id:
             products = Product.objects.filter(category=Category.objects.get(id=category_id))
-        else:
-            products = Product.objects.all()
+
         context = {
             'products': products,
-            'users' : request.user
+            'user': request.user,
+            'max_page': range(1, max_page+1)
         }
         return render(request, 'products/products.html', context=context)
-
 
 def main_view(request):
     if request.method == 'GET':
